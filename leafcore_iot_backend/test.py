c7f2c2f0efe09b79ac0bd0f1fd9feadf093bfc28
olgabondos <bondosolga@gmail.com>
2026-01-08 01:57:07 +0100

    Remove leafcore_iot_backend submodule, add to .gitignore

--- leafcore_iot_backend/test.py
+++
@@ -1,9 +0,0 @@
-
-import PySimpleGUI as sg
-print("Loaded from:", getattr(sg, "__file__", None))
-print("Version:", getattr(sg, "__version__", "unknown"))
-print("Has Text?", hasattr(sg, "Text"))
-
-layout = [[sg.Text("Hello")],[sg.Button("OK")]]
-sg.Window("PSG test", layout).read(close=True)
-
